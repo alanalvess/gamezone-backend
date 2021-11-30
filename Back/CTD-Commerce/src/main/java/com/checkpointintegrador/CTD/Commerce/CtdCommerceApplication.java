1da8f12b057e7f88898ceffc55e5e65c66811d24
Alan <alan.alvess@outlook.com>
2021-11-30 12:41:06 -0300

    Iniciando projeto Mavem

---
+++ Back/CTD-Commerce/src/main/java/com/checkpointintegrador/CTD/Commerce/CtdCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.checkpointintegrador.CTD.Commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtdCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtdCommerceApplication.class, args);
+	}
+
+}
---
+++ Back/CTD-Commerce/src/test/java/com/checkpointintegrador/CTD/Commerce/CtdCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkpointintegrador.CTD.Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtdCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
