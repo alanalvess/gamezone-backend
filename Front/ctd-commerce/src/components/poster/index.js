87c5e121ed83227beffa0c00c472dbabbff54b18
Viviansol <viviansan93@gmail.com>
2021-12-01 10:30:21 -0300

    inicio front

---
+++ Front/ctd-commerce/src/components/poster/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.scss'
+const Poster = () => {
+    return (
+        <div>
+            <section id="poster"></section>
+        </div>
+    )
+}
+
+export default Poster
