f87d945aea541e375b9f6ea2e37f8fa5ecf9a489
Bruno Barbosa <brb.dev@outlook.com>
2021-12-01 09:06:12 -0400

    some changes

--- Back/CTD-Commerce/src/main/java/com/checkpointintegrador/CTD/Commerce/controller/ProductController.java
+++ Back/CTD-Commerce/src/main/java/com/checkpointintegrador/CTD/Commerce/controller/ProductController.java
@@ -2,45 +2,50 @@ package com.checkpointintegrador.CTD.Commerce.controller;
 
 import com.checkpointintegrador.CTD.Commerce.model.Category;
 import com.checkpointintegrador.CTD.Commerce.model.Product;
-import com.checkpointintegrador.CTD.Commerce.service.CategoryService;
-import com.checkpointintegrador.CTD.Commerce.service.ProductService;
+import com.checkpointintegrador.CTD.Commerce.repository.CategoryRepository;
+import com.checkpointintegrador.CTD.Commerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
-    ProductService productService;
-    CategoryService categoryService;
+    ProductRepository productRepository;
+    CategoryRepository categoryRepository;
 
     @Autowired
-    public ProductController(ProductService productService){
-        this.productService = productService;
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
     }
 
     @PostMapping()
-    public ResponseEntity<Product> registerProduct(@RequestBody Product product){
-        return ResponseEntity.ok(productService.registerProduct(product));
+    private ResponseEntity<Product> registerProduct(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(product));
     }
 
     @GetMapping
-    public ResponseEntity<List<Product>> listProducts(){
-        return ResponseEntity.ok(productService.listProducts());
+    private ResponseEntity<List<Product>> listAllProducts() {
+        return ResponseEntity.ok(productRepository.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> findProduct(@PathVariable Integer id){
-        Product product = productService.findProduct(id).orElse(null);
-
-        return ResponseEntity.ok(product);
+    private ResponseEntity<Optional<Product>> findProductById(@PathVariable Integer id) {
+        return ResponseEntity.ok(productRepository.findById(id));
     }
 
     @GetMapping("/categories")
-    public ResponseEntity<List<Category>> listCategories(){
-        return ResponseEntity.ok((categoryService.listCategories()));
+    private  ResponseEntity<List<Category>> listAllCategories() {
+        return ResponseEntity.ok(categoryRepository.findAll());
+    }
+
+    @GetMapping("/category/{name}")
+    private ResponseEntity<List<Product>> listProductsByCategory(@PathVariable String name) {
+        return ResponseEntity.ok(categoryRepository.findByCategoryName(name));
     }
 }
--- Back/CTD-Commerce/src/main/java/com/checkpointintegrador/CTD/Commerce/repository/CategoryRepository.java
+++ Back/CTD-Commerce/src/main/java/com/checkpointintegrador/CTD/Commerce/repository/CategoryRepository.java
@@ -1,9 +1,19 @@
 package com.checkpointintegrador.CTD.Commerce.repository;
 
 import com.checkpointintegrador.CTD.Commerce.model.Category;
+import com.checkpointintegrador.CTD.Commerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    @Query("SELECT p FROM Product p WHERE p.category.name = ?1")
+    List<Product> findByCategoryName(String name);
 }
