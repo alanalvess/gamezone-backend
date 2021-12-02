dc5b5623bf324d6b4f0312ac406e416f474e97fe
Alan <alan.alvess@outlook.com>
2021-12-02 12:30:09 -0300

    Corrigindo o poster e o tamanho das imagens

--- Front/ctd-commerce/src/components/poster/index.js
+++ Front/ctd-commerce/src/components/poster/index.js
@@ -5,54 +5,63 @@ import img1 from '../../img/posters/poster-1.jpg'
 import img2 from '../../img/posters/poster-2.jpg'
 import img3 from '../../img/posters/poster-3.jpg'
 import img4 from '../../img/posters/poster-4.jpg'
+import img5 from '../../img/posters/poster-5.jpg'
+
 const Poster = () => {
     return (
-        <Carousel fade controls={false} indicators={false}>
+        <Carousel id="poster" fade controls={false} indicators={false}>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="poster-img d-block w-100"
                     src={img1}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h2>God of Wars</h2>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="poster-img d-block w-100"
                     src={img2}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h2>GTA5</h2>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="poster-img d-block w-100"
                     src={img3}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h2>Need For Speed 2021</h2>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
+                    className="poster-img d-block w-100"
                     src={img4}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h2>Bully</h2>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="poster-img d-block w-100"
+                    src={img5}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h2>Scorpion</h2>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
